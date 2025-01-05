93a209769c0640695399a65befda04e55ebaeb7e
sebe <sebe2k04@gmail.com>
2025-01-05 12:33:01 +0530

    auth ui added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import {  Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Pro Hunt",
@@ -29,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` inter vsc-initialized antialiased`}
+        className={` ${inter} vsc-initialized antialiased`}
       >
         <ThemeProvider>
           <div className="dark:bg-[#121212] dark:text-gray-300 text-gray-700  ">
